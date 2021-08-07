2624676751ada708449744632ed0715ba4408896
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-07 19:56:09 +0200

    dashboard/category: removed spinner

--- src/app/dashboard/category/category.component.ts
+++ src/app/dashboard/category/category.component.ts
@@ -18,14 +18,6 @@ export class CategoryComponent {
     return technology.featureDetections.map(det => det.detection).filter(Boolean).length;
   }
 
-  getCompatibleFeaturesPercentage(technology: Technology): number {
-    const detections = technology.featureDetections.map(det => det.detection);
-    if (detections.length > 0) {
-      return (this.getCompatibleFeaturesCount(technology) / detections.length) * 100;
-    }
-    return 0;
-  }
-
   getRightIcon(technology: Technology): string {
     if (technology.featureDetections.length > 0) {
       return this.getCompatibleFeaturesCount(technology) > 0 ? 'check' : 'error_outline';
