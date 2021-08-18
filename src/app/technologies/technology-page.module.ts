5c1d8ea4f89cf5e1f7174fff651cbde82e8d8a55
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-18 17:04:05 +0200

    TechnologyModule: use own demo component only if demo implemented

--- src/app/technologies/technology-page.module.ts
+++ src/app/technologies/technology-page.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import {Routes} from '@angular/router';
-import * as TECHNOLOGIES from './technologies';
+import {Route, Routes} from '@angular/router';
 import {AccelerometerComponent} from './demos/accelerometer/accelerometer.component';
 import {AmbientLightSensorComponent} from './demos/ambient-light-sensor/ambient-light-sensor.component';
 import {DeviceorientationEventComponent} from './demos/deviceorientation-event/deviceorientation-event.component';
@@ -72,315 +71,66 @@ import {PaymentHandlerComponent} from './demos/payment-handler/payment-handler.c
 import {WebotpComponent} from './demos/webotp/webotp.component';
 import {DemoFrameComponent} from '../demo-frame/demo-frame.component';
 import {SharedModule} from '../shared/shared.module';
+import {
+  BADGING,
+  DEVICEORIENTATION_EVENT,
+  GEOLOCATION,
+  MEDIA_SESSION_STANDARD, NOTIFICATIONS,
+  ORIENTATION_SENSOR, SERVICE_WORKER,
+  VIBRATION,
+  WEB_APP_MANIFEST
+} from './technologies';
+import {TECHNOLOGIES} from '../shared/functions';
 
-const routesSensors: Routes = [
+const DEMO_ROUTES: Routes = [
   {
-    path: TECHNOLOGIES.ACCELEROMETER.routerLink,
-    component: AccelerometerComponent
-  },
-  {
-    path: TECHNOLOGIES.AMBIENT_LIGHT_SENSOR.routerLink,
-    component: AmbientLightSensorComponent
-  },
-  {
-    path: TECHNOLOGIES.DEVICEORIENTATION_EVENT.routerLink,
-    component: DeviceorientationEventComponent
-  },
-  {
-    path: TECHNOLOGIES.GEOLOCATION.routerLink,
-    component: GeolocationComponent
-  },
-  {
-    path: TECHNOLOGIES.GEOLOCATION_SENSOR.routerLink,
-    component: GeolocationSensorComponent
-  },
-  {
-    path: TECHNOLOGIES.GYROSCOPE.routerLink,
-    component: GyroscopeComponent
-  },
-  {
-    path: TECHNOLOGIES.MAGNETOMETER.routerLink,
-    component: MagnetometerComponent
-  },
-  {
-    path: TECHNOLOGIES.ORIENTATION_SENSOR.routerLink,
-    component: OrientationSensorComponent
-  },
-  {
-    path: TECHNOLOGIES.PROXIMITY_SENSOR.routerLink,
-    component: ProximitySensorComponent
-  },
-];
-const routesDeviceInfos: Routes = [
-  {
-    path: TECHNOLOGIES.BATTERY_STATUS.routerLink,
-    component: BatteryStatusComponent
-  },
-  {
-    path: TECHNOLOGIES.COMPUTE_PRESSURE.routerLink,
-    component: ComputePressureComponent
-  },
-  {
-    path: TECHNOLOGIES.DEVICE_MEMORY.routerLink,
-    component: DeviceMemoryComponent
-  },
-  {
-    path: TECHNOLOGIES.DEVICE_POSTURE.routerLink,
-    component: DevicePostureComponent
-  },
-  {
-    path: TECHNOLOGIES.MEDIA_CAPABILITIES.routerLink,
-    component: MediaCapabilitiesComponent
-  },
-  {
-    path: TECHNOLOGIES.NETWORK_INFORMATION.routerLink,
-    component: NetworkInformationComponent
-  },
-];
-const routesScreen: Routes = [
-  {
-    path: TECHNOLOGIES.FULLSCREEN.routerLink,
-    component: FullscreenComponent
-  },
-  {
-    path: TECHNOLOGIES.MULTI_SCREEN_WINDOW_PLACEMENT.routerLink,
-    component: MultiScreenWindowPlacementComponent
-  },
-  {
-    path: TECHNOLOGIES.PRESENTATION.routerLink,
-    component: PresentationComponent
-  },
-  {
-    path: TECHNOLOGIES.SCREEN_ORIENTATION.routerLink,
-    component: ScreenOrientationComponent
-  },
-  {
-    path: TECHNOLOGIES.SCREEN_WAKE_LOCK.routerLink,
-    component: ScreenWakeLockComponent
-  }
-];
-const routesCamMicCapture: Routes = [
-  {
-    path: TECHNOLOGIES.HTML_MEDIA_CAPTURE.routerLink,
-    component: HtmlMediaCaptureComponent
-  },
-  {
-    path: TECHNOLOGIES.MEDIA_CAPTURE_AND_STREAMS.routerLink,
-    component: MediaCaptureAndStreamsComponent
-  },
-  {
-    path: TECHNOLOGIES.MEDIA_CAPTURE_FROM_DOM_ELEMENTS.routerLink,
-    component: MediaCaptureFromDomElementsComponent
-  },
-  {
-    path: TECHNOLOGIES.MEDIASTREAM_IMAGE_CAPTURE.routerLink,
-    component: MediastreamImageCaptureComponent
-  },
-  {
-    path: TECHNOLOGIES.MEDIASTREAM_RECORDING.routerLink,
-    component: MediastreamRecordingComponent
-  },
-  {
-    path: TECHNOLOGIES.SCREEN_CAPTURE.routerLink,
-    component: ScreenCaptureComponent
-  },
-  {
-    path: TECHNOLOGIES.SHAPE_DETECTION.routerLink,
-    component: ShapeDetectionComponent
-  },
-  {
-    path: TECHNOLOGIES.TEXT_DETECTION.routerLink,
-    component: TextDetectionComponent
-  }
-];
-const routesInputOutputDevices: Routes = [
-  {
-    path: TECHNOLOGIES.AUDIO_OUTPUT_DEVICES.routerLink,
-    component: AudioOutputDevicesComponent
-  },
-  {
-    path: TECHNOLOGIES.GAMEPAD.routerLink,
-    component: GamepadComponent
-  },
-  {
-    path: TECHNOLOGIES.REMOTE_PLAYBACK.routerLink,
-    component: RemotePlaybackComponent
-  },
-  {
-    path: TECHNOLOGIES.WEB_MIDI.routerLink,
-    component: WebMidiComponent
-  },
-  {
-    path: TECHNOLOGIES.WEBHID.routerLink,
-    component: WebhidComponent
-  },
-  {
-    path: TECHNOLOGIES.WEBXR_DEVICE.routerLink,
-    component: WebxrDeviceComponent
-  }
-];
-const routesInputProcessing: Routes = [
-  {
-    path: TECHNOLOGIES.INPUT_DEVICE_CAPABILITIES.routerLink,
-    component: InputDeviceCapabilitiesComponent
-  },
-  {
-    path: TECHNOLOGIES.KEYBOARD_LOCK.routerLink,
-    component: KeyboardLockComponent
-  },
-  {
-    path: TECHNOLOGIES.KEYBOARD_MAP.routerLink,
-    component: KeyboardMapComponent
-  },
-  {
-    path: TECHNOLOGIES.POINTER_EVENTS.routerLink,
-    component: PointerEventsComponent
-  },
-  {
-    path: TECHNOLOGIES.POINTER_LOCK.routerLink,
-    component: PointerLockComponent
-  },
-  {
-    path: TECHNOLOGIES.TOUCH_EVENTS.routerLink,
-    component: TouchEventsComponent
-  },
-  {
-    path: TECHNOLOGIES.VIRTUALKEYBOARD.routerLink,
-    component: VirtualkeyboardComponent
-  },
-  {
-    path: TECHNOLOGIES.WEB_SPEECH.routerLink,
-    component: WebSpeechComponent
-  }
-];
-const routesFurtherHardwareAndPeripheral: Routes = [
-  {
-    path: TECHNOLOGIES.VIBRATION.routerLink,
-    component: VibrationComponent
-  },
-  {
-    path: TECHNOLOGIES.WEB_BLUETOOTH.routerLink,
-    component: WebBluetoothComponent
-  },
-  {
-    path: TECHNOLOGIES.WEB_NFC.routerLink,
-    component: WebNfcComponent
-  },
-  {
-    path: TECHNOLOGIES.WEB_SERIAL.routerLink,
-    component: WebSerialComponent
-  },
-  {
-    path: TECHNOLOGIES.WEBUSB.routerLink,
-    component: WebusbComponent
-  }
-];
-const routesLocalResources: Routes = [
-  {
-    path: TECHNOLOGIES.CONTACT_PICKER.routerLink,
-    component: ContactPickerComponent
-  },
-  {
-    path: TECHNOLOGIES.CONTENT_INDEX.routerLink,
-    component: ContentIndexComponent
-  },
-  {
-    path: TECHNOLOGIES.FILE.routerLink,
-    component: FileComponent
-  },
-  {
-    path: TECHNOLOGIES.FILE_SYSTEM_ACCESS.routerLink,
-    component: FileSystemAccessComponent
-  },
-  {
-    path: TECHNOLOGIES.INDEXED_DATABASE.routerLink,
-    component: IndexedDatabaseComponent
-  },
-  {
-    path: TECHNOLOGIES.LOCAL_FONT_ACCESS.routerLink,
-    component: LocalFontAccessComponent
-  },
-  {
-    path: TECHNOLOGIES.SERVICE_WORKER.routerLink,
-    component: ServiceWorkerComponent
-  },
-  {
-    path: TECHNOLOGIES.STORAGE_STANDARD.routerLink,
-    component: StorageStandardComponent
-  }
-];
-const routesPlatformUI: Routes = [
-  {
-    path: TECHNOLOGIES.BADGING.routerLink,
+    path: BADGING.routerLink,
     component: BadgingComponent
   },
   {
-    path: TECHNOLOGIES.MEDIA_SESSION_STANDARD.routerLink,
+    path: MEDIA_SESSION_STANDARD.routerLink,
     component: MediaSessionStandardComponent
   },
   {
-    path: TECHNOLOGIES.NOTIFICATIONS.routerLink,
-    component: NotificationsComponent
-  },
-  {
-    path: TECHNOLOGIES.PAYMENT_REQUEST.routerLink,
-    component: PaymentRequestComponent
-  },
-  {
-    path: TECHNOLOGIES.PICTURE_IN_PICTURE.routerLink,
-    component: PictureInPictureComponent
-  },
-  {
-    path: TECHNOLOGIES.WEB_APP_MANIFEST.routerLink,
+    path: WEB_APP_MANIFEST.routerLink,
     component: WebAppManifestComponent
   },
   {
-    path: TECHNOLOGIES.WEB_AUTHENTICATION.routerLink,
-    component: WebAuthenticationComponent
+    path: SERVICE_WORKER.routerLink,
+    component: ServiceWorkerComponent
   },
   {
-    path: TECHNOLOGIES.WEB_SHARE.routerLink,
-    component: WebShareComponent
-  },
-];
-const routesFurtherPlatform: Routes = [
-  {
-    path: TECHNOLOGIES.CLIPBOARD.routerLink,
-    component: ClipboardComponent
+    path: NOTIFICATIONS.routerLink,
+    component: NotificationsComponent
   },
   {
-    path: TECHNOLOGIES.GET_INSTALLED_RELATED_APPS.routerLink,
-    component: GetInstalledRelatedAppsComponent
+    path: VIBRATION.routerLink,
+    component: VibrationComponent
   },
   {
-    path: TECHNOLOGIES.IDLE_DETECTION.routerLink,
-    component: IdleDetectionComponent
+    path: GEOLOCATION.routerLink,
+    component: GeolocationComponent
   },
   {
-    path: TECHNOLOGIES.PAGE_VISIBILITY.routerLink,
-    component: PageVisibilityComponent
+    path: DEVICEORIENTATION_EVENT.routerLink,
+    component: DeviceorientationEventComponent
   },
   {
-    path: TECHNOLOGIES.PAYMENT_HANDLER.routerLink,
-    component: PaymentHandlerComponent
-  },
-  {
-    path: TECHNOLOGIES.WEBOTP.routerLink,
-    component: WebotpComponent
+    path: ORIENTATION_SENSOR.routerLink,
+    component: OrientationSensorComponent
   },
 ];
 
 export const TECHNOLOGYPAGE_ROUTES: Routes = [
-  ...routesSensors,
-  ...routesDeviceInfos,
-  ...routesScreen,
-  ...routesCamMicCapture,
-  ...routesInputOutputDevices,
-  ...routesInputProcessing,
-  ...routesFurtherHardwareAndPeripheral,
-  ...routesLocalResources,
-  ...routesPlatformUI,
-  ...routesFurtherPlatform,
+  ...DEMO_ROUTES,
+  // All Technologies which are not in DEMO_ROUTES
+    ...TECHNOLOGIES.filter(technology => !DEMO_ROUTES.map(d => d.path).includes(technology.routerLink)).map(res => {
+    return {
+      path: res.routerLink,
+      component: DemoFrameComponent,
+      data: {technology: res}
+    } as Route;
+  }),
   // {
   //   path: TECHNOLOGIES.VIBRATION.routerLink,
   //   component: DemoFrameComponent,
