fcdaa339f62fbd36c48e1a7bb153089da85b5cb4
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-19 16:26:22 +0200

    PictureInPicture: basic demo

--- src/app/technologies/demos/picture-in-picture/picture-in-picture.component.ts
+++ src/app/technologies/demos/picture-in-picture/picture-in-picture.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// @ts-nocheck
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -6,6 +7,27 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   templateUrl: './picture-in-picture.component.html',
   styleUrls: ['./picture-in-picture.component.css']
 })
-export class PictureInPictureComponent extends TechnologyDemoComponent {
 
+export class PictureInPictureComponent extends TechnologyDemoComponent {
+  @ViewChild('videoElement', { static: false })
+  set videoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.videoElement = ref.nativeElement;
+  }
+  videoElement: HTMLVideoElement;
+
+  togglePicInPic(): void {
+    if ('pictureInPictureEnabled' in document) {
+      if (document.pictureInPictureEnabled || !this.videoElement.disablePictureInPicture) {
+        if (document.pictureInPictureElement) {
+          document.exitPictureInPicture().catch(err => this.showError(err));
+        } else {
+          this.videoElement.requestPictureInPicture().catch(err => this.showError(err));
+        }
+      } else {
+        this.showError('Bild-in-Bild ist deaktiviert!');
+      }
+    } else {
+      this.showNoSupportError('document.pictureInPictureEnabled');
+    }
+  }
 }
