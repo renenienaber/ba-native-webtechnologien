1a170de133b3f5deedc05b22ae3363de852068c3
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-19 18:17:03 +0200

    WebAuthentication: created demo for PublicKey-creation

--- src/app/technologies/demos/web-authentication/web-authentication.component.ts
+++ src/app/technologies/demos/web-authentication/web-authentication.component.ts
@@ -7,4 +7,37 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./web-authentication.component.css']
 })
 export class WebAuthenticationComponent extends TechnologyDemoComponent {
+  publicKey: PublicKeyCredentialCreationOptions = {
+    // random, cryptographically secure, at least 16 bytes, sent from server
+    challenge: new Uint8Array(16),
+    // relying party
+    rp: {
+      name: 'Awesome Corp', // sample relying party
+    },
+    user: {
+      id: new Uint8Array(16),
+      name: 'username@beispiel.de',
+      displayName: 'Max Mustermann',
+    },
+    authenticatorSelection: { userVerification: 'preferred' },
+    attestation: 'direct',
+    pubKeyCredParams: [
+      {
+        type: 'public-key',
+        alg: -7, // "ES256" IANA COSE Algorithms registry
+      },
+    ],
+  };
+
+  cred: PublicKeyCredential;
+
+  createCredential(): void {
+    navigator.credentials.create({ publicKey: this.publicKey })
+      .then(res => {
+        // Send data to relying party's servers
+        this.cred = res as PublicKeyCredential;
+        this.showError('Public Key wurde erstellt. Es würde auf einem Server hinterlegt werden. Zur Demonstration wird er hier temporär gespeichert');
+      })
+      .catch(err => this.showError(err));
+  }
 }
