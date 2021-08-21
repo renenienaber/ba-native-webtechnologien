134243312f9c7948535d0a5f4677f99ea8d40dc2
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-21 20:01:47 +0200

    ContentIndex: basic demo for adding index

--- src/app/technologies/demos/content-index/content-index.component.ts
+++ src/app/technologies/demos/content-index/content-index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -6,5 +6,72 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   templateUrl: './content-index.component.html',
   styleUrls: ['./content-index.component.css']
 })
-export class ContentIndexComponent extends TechnologyDemoComponent {
+export class ContentIndexComponent extends TechnologyDemoComponent implements OnInit {
+  registration: ServiceWorkerRegistration;
+  isSupported = false;
+
+  allContents: any[] = [];
+  predefinedContentDtos: any[] = [
+    {
+      id: 'dashboard',
+      url: '/dashboard',
+      title: 'Übersicht',
+      description: 'Eine Übersicht über alle nativen Webtechnologien',
+    },
+    {
+      id: 'about',
+      url: '/about',
+      title: 'Informationen',
+      description: 'Informationen zu dieser Anwendung',
+    }
+  ];
+
+  newContentDto = {...this.predefinedContentDtos[0]};
+
+  ngOnInit(): void {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(res => {
+        this.registration = res;
+        if ('index' in this.registration) {
+          this.isSupported = true;
+          this.updateContents();
+        }
+      });
+    }
+  }
+
+  private updateContents(): void {
+    // @ts-ignore
+    this.registration.index.getAll()
+      .then(indexContent => this.allContents = indexContent)
+      .catch(err => this.showError(err));
+  }
+
+  onSelectionChanged(obj: any): void {
+    this.newContentDto = {...obj};
+  }
+
+  addToIndex(): void {
+    if (this.registration) {
+      if (this.isSupported) {
+        // @ts-ignore
+        this.registration.index.add({
+          id: this.newContentDto.id,
+          url: this.newContentDto.url,
+          title: this.newContentDto.title,
+          description: this.newContentDto.description,
+          icons: [{
+            src: './assets/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          }],
+        }).then(() => this.updateContents())
+          .catch(err => this.showError(err));
+      } else {
+        this.showNoSupportError('ServiceWorkerRegistration.index');
+      }
+    } else {
+      this.showError('Es wurde kein Service Worker registriert. Bitte die App installieren und noch einmal versuchen.');
+    }
+  }
 }
