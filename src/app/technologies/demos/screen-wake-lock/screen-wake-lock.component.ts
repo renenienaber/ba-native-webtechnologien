4ccfadadff60797f238b220bb3c62b1898e051d6
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-21 15:39:41 +0200

    ScreenWakeLock: simple demo

--- src/app/technologies/demos/screen-wake-lock/screen-wake-lock.component.ts
+++ src/app/technologies/demos/screen-wake-lock/screen-wake-lock.component.ts
@@ -7,4 +7,14 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./screen-wake-lock.component.css']
 })
 export class ScreenWakeLockComponent extends TechnologyDemoComponent {
+  requestWakeLock(): void {
+    if ('wakeLock' in navigator) {
+      // @ts-ignore
+      navigator.wakeLock.request()
+        .then(() => this.showError('WakeLock wurde aktiviert!'))
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('navigator.wakeLock');
+    }
+  }
 }
