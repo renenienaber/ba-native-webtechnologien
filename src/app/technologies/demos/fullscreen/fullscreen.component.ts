82403051f8ad67743db8fbdf36c40567d7f8be08
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-21 13:14:23 +0200

    Fullscreen: simple video demo

--- src/app/technologies/demos/fullscreen/fullscreen.component.ts
+++ src/app/technologies/demos/fullscreen/fullscreen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -7,4 +7,22 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./fullscreen.component.css']
 })
 export class FullscreenComponent extends TechnologyDemoComponent {
+  @ViewChild('videoElement', { static: false })
+  set videoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.videoElement = ref.nativeElement;
+  }
+  videoElement: HTMLVideoElement;
+
+  activateFullscreen(): void {
+    if ('requestFullscreen' in this.videoElement) {
+      if (!document.fullscreenElement) {
+        this.videoElement.requestFullscreen()
+          .catch(err => this.showError(err));
+      } else {
+        this.showError('Vollbildmodus ist bereits für ein Element aktiviert.');
+      }
+    } else {
+      this.showNoSupportError('Element.requestFullscreen');
+    }
+  }
 }
