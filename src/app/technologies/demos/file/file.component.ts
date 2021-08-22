da9f3c1e18afd083db809c0e2394493c871a335f
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 11:37:27 +0200

    FileAPI: demo for reading txt files with FileReader

--- src/app/technologies/demos/file/file.component.ts
+++ src/app/technologies/demos/file/file.component.ts
@@ -7,4 +7,30 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./file.component.css']
 })
 export class FileComponent extends TechnologyDemoComponent {
+  fileList;
+  result;
+
+  onFileInputChanged(eventTarget: any): void {
+    this.fileList = undefined;
+    this.result = undefined;
+    if (eventTarget.files.length > 0) {
+      this.fileList = eventTarget.files;
+      this.updateResult();
+    }
+  }
+
+  private updateResult(): void {
+    const file: File = this.fileList[0];
+    const textType = /text.*/;
+
+    if (file.type.match(textType)) {
+      if ('FileReader' in window) {
+        const fileReader = new FileReader();
+        fileReader.readAsText(file);
+        fileReader.onload = () => this.result = fileReader.result;
+      } else {
+        this.showNoSupportError('window.FileReader');
+      }
+    }
+  }
 }
