e79961c516c4c86532151ce0610cd5fab3b7a1af
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 10:34:10 +0200

    FileSystemAccess: texteditor-demo for each main function

--- src/app/technologies/demos/file-system-access/file-system-access.component.ts
+++ src/app/technologies/demos/file-system-access/file-system-access.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
   selector: 'app-file-system-access',
   templateUrl: './file-system-access.component.html',
   styleUrls: ['./file-system-access.component.css']
 })
-export class FileSystemAccessComponent implements OnInit {
+export class FileSystemAccessComponent extends TechnologyDemoComponent {
+  editor = 'Dies ist ein Textinhalt.';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onShowOpenFilePicker(): void {
+    if ('showOpenFilePicker' in window) {
+      // @ts-ignore
+      window.showOpenFilePicker()
+        .then(res => res[0])
+        .then(fileHandle => fileHandle.getFile())
+        .then(file => file.text())
+        .then(contents => this.editor = contents)
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('window.showOpenFilePicker()');
+    }
   }
 
+  onShowSaveFilePicker(): void {
+    if ('showSaveFilePicker' in window) {
+      const options = {
+        types: [{
+          description: 'Textdateien',
+          accept: {'text/plain' : ['.txt']}
+        }]
+      };
+      // @ts-ignore
+      window.showSaveFilePicker(options)
+        .then(handle => handle.createWritable())
+        .then(writable => {
+          writable.write(this.editor)
+            .then(() => writable.close());
+        })
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('window.showSaveFilePicker()');
+    }
+  }
+
+  onShowDirectoryPicker(): void {
+    if ('showDirectoryPicker' in window) {
+      // @ts-ignore
+      window.showDirectoryPicker()
+        .then(dirHandle => dirHandle.getFileHandle('File System Access Demo.txt', { create: true }))
+        .then(handle => handle.createWritable())
+        .then(writable => {
+          writable.write(this.editor)
+            .then(() => writable.close());
+        })
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('window.showDirectoryPicker()');
+    }
+  }
 }
