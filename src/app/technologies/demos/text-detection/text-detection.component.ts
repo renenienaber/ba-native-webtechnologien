21691f45038188ba5c7b99d9de07e140f887732f
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 21:08:58 +0200

    TextDetection: simple demo (like ShapeDetection)

--- src/app/technologies/demos/text-detection/text-detection.component.ts
+++ src/app/technologies/demos/text-detection/text-detection.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
   selector: 'app-text-detection',
   templateUrl: './text-detection.component.html',
   styleUrls: ['./text-detection.component.css']
 })
-export class TextDetectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TextDetectionComponent extends TechnologyDemoComponent {
+  @ViewChild('textImgElement', { static: false })
+  set textImgRef(ref: ElementRef<HTMLImageElement>) {
+    this.textImgElement = ref.nativeElement;
   }
+  textImgElement: HTMLImageElement;
 
+  results;
+
+  detectText(): void {
+    if ('TextDetector' in window) {
+      // @ts-ignore
+      const textDetector = new TextDetector();
+      // @ts-ignore
+      if ('detect' in textDetector) {
+        textDetector.detect(this.textImgElement)
+          .then(res => this.results = res)
+          .catch(err => this.showError(err));
+      } else {
+        this.showNoSupportError('TextDetector.detect()');
+      }
+    } else {
+      this.showNoSupportError('window.TextDetector');
+    }
+  }
 }
