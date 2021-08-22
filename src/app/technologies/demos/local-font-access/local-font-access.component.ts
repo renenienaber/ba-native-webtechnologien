35d69312a88e7bda040e54cdee7cea6c67ce66eb
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 11:00:09 +0200

    LocalFontAccess: simple demo for selecting and showing font information

--- src/app/technologies/demos/local-font-access/local-font-access.component.ts
+++ src/app/technologies/demos/local-font-access/local-font-access.component.ts
@@ -7,4 +7,22 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./local-font-access.component.css']
 })
 export class LocalFontAccessComponent extends TechnologyDemoComponent {
+  result = [];
+
+  getFonts(): void {
+    if ('fonts' in navigator) {
+      // @ts-ignore
+      navigator.fonts.query()
+        .then(res => {
+          res.forEach(e => this.result.push({
+            postscriptName: e.postscriptName,
+            fullName: e.fullName,
+            family: e.family
+          }));
+        })
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('navigator.fonts');
+    }
+  }
 }
