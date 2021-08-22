38170b25b2023697d37fac53d65121276d2276df
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 20:31:59 +0200

    ShapeDetection: added demos for both bacode & face detection

--- src/app/technologies/demos/shape-detection/shape-detection.component.ts
+++ src/app/technologies/demos/shape-detection/shape-detection.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -7,4 +7,52 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./shape-detection.component.css']
 })
 export class ShapeDetectionComponent extends TechnologyDemoComponent {
+  @ViewChild('barcodeImgElement', { static: false })
+  set barcodeImgRef(ref: ElementRef<HTMLImageElement>) {
+    this.barcodeImgElement = ref.nativeElement;
+  }
+  @ViewChild('faceImgElement', { static: false })
+  set faceImgRef(ref: ElementRef<HTMLImageElement>) {
+    this.faceImgElement = ref.nativeElement;
+  }
+
+  barcodeImgElement: HTMLImageElement;
+  faceImgElement: HTMLImageElement;
+
+  barcodeResults;
+  faceResults;
+
+  detectBarcode(): void {
+    if('BarcodeDetector' in window) {
+      // @ts-ignore
+      const barcodeDetector = new BarcodeDetector();
+      // @ts-ignore
+      if('detect' in barcodeDetector) {
+        barcodeDetector.detect()
+          .then(res => this.barcodeResults = res)
+          .catch(err => this.showError(err));
+      } else {
+        this.showNoSupportError('BarcodeDetector.detect()');
+      }
+    } else {
+      this.showNoSupportError('window.BarcodeDetector');
+    }
+  }
+
+  detectFaces(): void {
+    if('FaceDetector' in window) {
+      // @ts-ignore
+      const faceDetector = new FaceDetector();
+      // @ts-ignore
+      if('detect' in faceDetector) {
+        faceDetector.detect()
+          .then(res => this.faceResults = res)
+          .catch(err => this.showError(err));
+      } else {
+        this.showNoSupportError('FaceDetector.detect()');
+      }
+    } else {
+      this.showNoSupportError('window.FaceDetector');
+    }
+  }
 }
