7ff701e3bb2805b968560d3ae815383f73dc1995
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 18:57:31 +0200

    ScreenCapture: added simple demo with displaying injected MediaStream

--- src/app/technologies/demos/screen-capture/screen-capture.component.ts
+++ src/app/technologies/demos/screen-capture/screen-capture.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -7,4 +7,26 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./screen-capture.component.css']
 })
 export class ScreenCaptureComponent extends TechnologyDemoComponent {
+  @ViewChild('videoElement', { static: false })
+  set videoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.videoElement = ref.nativeElement;
+  }
+  videoElement: HTMLVideoElement;
+
+  private isSupported(): boolean {
+    if ('mediaDevices' in navigator && 'getDisplayMedia' in navigator.mediaDevices) {
+      return true;
+    }
+    this.showNoSupportError('navigator.mediaDevices.getDisplayMedia()');
+    return false;
+  }
+
+  getDisplayMedia(): void {
+    if (this.isSupported()) {
+      // @ts-ignore
+      navigator.mediaDevices.getDisplayMedia({ video: true })
+        .then(stream => this.videoElement.srcObject = stream)
+        .catch(err => this.showError(err));
+    }
+  }
 }
