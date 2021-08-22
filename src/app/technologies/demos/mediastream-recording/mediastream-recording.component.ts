42790ed3c17c6c6601fddada4cfa0a4ef277edd4
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 18:31:30 +0200

    MediaStreamRecording: recording video demo under usage of getUserMedia

--- src/app/technologies/demos/mediastream-recording/mediastream-recording.component.ts
+++ src/app/technologies/demos/mediastream-recording/mediastream-recording.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -7,5 +7,65 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./mediastream-recording.component.css']
 })
 export class MediastreamRecordingComponent extends TechnologyDemoComponent {
+  @ViewChild('videoElement', { static: false })
+  set sourceVideoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.videoElement = ref.nativeElement;
+  }
+  @ViewChild('recordedVideoElement', { static: false })
+  set videoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.recordedVideoElement = ref.nativeElement;
+  }
 
+  videoElement: HTMLVideoElement;
+  recordedVideoElement: HTMLVideoElement;
+  mediaRecorder;
+  isRecording = false;
+  recordedBlobs: any[] = [];
+
+  getVideo(): void {
+    if ('mediaDevices' in navigator) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(stream => {
+          this.videoElement.srcObject = stream;
+          if ('MediaRecorder' in window) {
+            // @ts-ignore
+             this.mediaRecorder = new MediaRecorder(stream);
+          } else {
+            this.showNoSupportError('window.MediaRecorder');
+          }
+        })
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('navigator.mediaDevices');
+    }
+  }
+
+  private onRecorderDataAvailable(event: any) {
+    if (event.data && event.data.size > 0) {
+      this.recordedBlobs.push(event.data);
+      this.showRecordedVideo();
+    }
+  }
+
+  private showRecordedVideo(): void {
+    const blob = new Blob(this.recordedBlobs, { type: 'video/webm' });
+    const url = URL.createObjectURL(blob);
+    this.recordedVideoElement.src = url;
+  }
+
+  startRecording(): void {
+    if (this.mediaRecorder) {
+      this.recordedBlobs = [];
+      this.mediaRecorder.addEventListener('dataavailable', ev => this.onRecorderDataAvailable(ev));
+      this.mediaRecorder.start();
+      this.isRecording = true;
+    } else {
+      this.showNoSupportError('window.MediaRecorder');
+    }
+  }
+
+  stopRecording(): void {
+    this.isRecording = false;
+    this.mediaRecorder.stop();
+  }
 }
