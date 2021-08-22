6208516dc550bc5e97b513f4ac1ffdc34e3d64a9
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 14:20:13 +0200

    MediaCaptureAndStreams: basic demo for getting and play video and audio as MediaStream

--- src/app/technologies/demos/media-capture-and-streams/media-capture-and-streams.component.ts
+++ src/app/technologies/demos/media-capture-and-streams/media-capture-and-streams.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -7,4 +7,39 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./media-capture-and-streams.component.css']
 })
 export class MediaCaptureAndStreamsComponent extends TechnologyDemoComponent {
+  @ViewChild('videoElement', { static: false })
+  set videoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.videoElement = ref.nativeElement;
+  }
+  @ViewChild('audioElement', { static: false })
+  set audioRef(ref: ElementRef<HTMLAudioElement>) {
+    this.audioElement = ref.nativeElement;
+  }
+
+  audioElement: HTMLAudioElement;
+  videoElement: HTMLVideoElement;
+
+  private isMediaDevicesSupported(): boolean {
+    if ('mediaDevices' in navigator) {
+      return true;
+    }
+    this.showNoSupportError('navigator.mediaDevices');
+    return false;
+  }
+
+  onVideoCapture(): void {
+    if (this.isMediaDevicesSupported()) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => this.videoElement.srcObject = stream)
+        .catch(err => this.showError(err));
+    }
+  }
+
+  onAudioCapture(): void {
+    if (this.isMediaDevicesSupported()) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => this.audioElement.srcObject = stream)
+        .catch(err => this.showError(err));
+    }
+  }
 }
