6d3dfe564eb566d85721dfa96d0126e8fbedfd22
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 12:47:18 +0200

    HtmlMediaCapture: initial commit

---
+++ src/app/technologies/demos/html-media-capture/html-media-capture.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {TechnologyDemoComponent} from '../../technology-demo.component';
+
+@Component({
+  selector: 'app-html-media-capture',
+  templateUrl: './html-media-capture.component.html',
+  styleUrls: ['./html-media-capture.component.css']
+})
+export class HtmlMediaCaptureComponent extends TechnologyDemoComponent {
+}
--- src/app/technologies/technologies.module.ts
+++ src/app/technologies/technologies.module.ts
@@ -1,7 +1,7 @@
 import {
   BADGING, CONTACT_PICKER, CONTENT_INDEX,
   DEVICEORIENTATION_EVENT, FILE, FILE_SYSTEM_ACCESS, FULLSCREEN,
-  GEOLOCATION, LOCAL_FONT_ACCESS, MEDIA_SESSION_STANDARD, NOTIFICATIONS,
+  GEOLOCATION, HTML_MEDIA_CAPTURE, LOCAL_FONT_ACCESS, MEDIA_SESSION_STANDARD, NOTIFICATIONS,
   ORIENTATION_SENSOR, PAYMENT_REQUEST, PICTURE_IN_PICTURE, SCREEN_ORIENTATION, SCREEN_WAKE_LOCK,
   SERVICE_WORKER,
   VIBRATION,
@@ -37,6 +37,7 @@ import {MatChipsModule} from '@angular/material/chips';
 import { FileSystemAccessComponent } from './demos/file-system-access/file-system-access.component';
 import { LocalFontAccessComponent } from './demos/local-font-access/local-font-access.component';
 import { FileComponent } from './demos/file/file.component';
+import { HtmlMediaCaptureComponent } from './demos/html-media-capture/html-media-capture.component';
 
 const DEMO_COMPONENTS: {technology: Technology, component: any}[] = [
   {
@@ -70,6 +71,10 @@ const DEMO_COMPONENTS: {technology: Technology, component: any}[] = [
   {
     technology: GEOLOCATION,
     component: GeolocationComponent
+  },
+  {
+    technology: HTML_MEDIA_CAPTURE,
+    component: HtmlMediaCaptureComponent
   },
   {
     technology: LOCAL_FONT_ACCESS,
@@ -175,7 +180,8 @@ export const TECHNOLOGYPAGE_ROUTES: Routes = [
     ContentIndexComponent,
     FileSystemAccessComponent,
     LocalFontAccessComponent,
-    FileComponent
+    FileComponent,
+    HtmlMediaCaptureComponent
   ],
   imports: [
     SharedModule,
