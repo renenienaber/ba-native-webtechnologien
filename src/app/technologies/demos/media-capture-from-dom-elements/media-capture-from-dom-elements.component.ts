6a12a73761ce1bd17a1f3ed5ef076ade9d98e7bc
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-22 15:22:35 +0200

    MediaCaptureFromDomElements: simple demo with HTMLVideoElement

--- src/app/technologies/demos/media-capture-from-dom-elements/media-capture-from-dom-elements.component.ts
+++ src/app/technologies/demos/media-capture-from-dom-elements/media-capture-from-dom-elements.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -7,4 +7,25 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./media-capture-from-dom-elements.component.css']
 })
 export class MediaCaptureFromDomElementsComponent extends TechnologyDemoComponent {
+  @ViewChild('sourceVideoElement', { static: false })
+  set sourceVideoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.sourceVideoElement = ref.nativeElement;
+  }
+  @ViewChild('videoElement', { static: false })
+  set videoRef(ref: ElementRef<HTMLVideoElement>) {
+    this.videoElement = ref.nativeElement;
+  }
+
+  isSupported = false;
+  errorNoSupport = 'HTMLVideoElement.captureStream() wird nicht unterstützt!';
+
+  sourceVideoElement: HTMLVideoElement;
+  videoElement: HTMLVideoElement;
+
+  useInputVideoForOutputVideo(): void {
+    if ('captureStream' in this.sourceVideoElement) {
+      // @ts-ignore
+      this.videoElement.srcObject = this.sourceVideoElement.captureStream();
+    }
+  }
 }
