92ba8cad1b44ac3e7e218c6b13f251f362fdc385
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-23 18:38:33 +0200

    Gyroscope: simple demo

--- src/app/technologies/demos/gyroscope/gyroscope.component.ts
+++ src/app/technologies/demos/gyroscope/gyroscope.component.ts
@@ -7,5 +7,22 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   styleUrls: ['./gyroscope.component.css']
 })
 export class GyroscopeComponent extends TechnologyDemoComponent {
-  
+  sensor: any;
+
+  initAndStartSensor(): void {
+    if ('Gyroscope' in window) {
+      // @ts-ignore
+      this.sensor = new Gyroscope({frequency: 60});
+      // this.sensor.addEventListener('reading', () => {
+      //     this.results = {x: this.sensor.x, y: this.sensor.y, z: this.sensor.z};
+      //   }
+      // );
+      this.sensor.addEventListener('error', sensorErrorEvent =>
+        this.showError(sensorErrorEvent.error.message)
+      );
+      this.sensor.start();
+    } else {
+      this.showNoSupportError('window.Gyroscope');
+    }
+  }
 }
