9b0502905581d03352794bf61934ba1dc207c888
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-24 10:25:20 +0200

    OrientationSensor: fixed bug

--- src/app/technologies/demos/orientation-sensor/orientation-sensor.component.ts
+++ src/app/technologies/demos/orientation-sensor/orientation-sensor.component.ts
@@ -12,30 +12,27 @@ export class OrientationSensorComponent extends TechnologyDemoComponent {
   relativeMat4 = new Float32Array(16);
 
   initAndStartSensor(sensorType: 'absolute' | 'relative'): void {
-    if ('absolute') {
-      if (!('AbsoluteOrientationSensor' in window)) {
-        this.showNoSupportError('window.AbsoluteOrientationSensor');
-        return;
-      }
-    } else if ('relative') {
-      if (!('RelativeOrientationSensor' in window)) {
-        this.showNoSupportError('window.RelativeOrientationSensor');
-        return;
-      }
+    if ('absolute' && !('AbsoluteOrientationSensor' in window)) {
+      this.showNoSupportError('window.AbsoluteOrientationSensor');
+      return;
+    } else if ('relative' && !('RelativeOrientationSensor' in window)) {
+      this.showNoSupportError('window.RelativeOrientationSensor');
+      return;
     }
-    // @ts-ignore
-    const sensor = sensorType === 'absolute' ? new AbsoluteOrientationSensor({frequency: 60}) : new RelativeOrientationSensor({frequency: 60});
+
+    const sensor =
+      // @ts-ignore
+      sensorType === 'absolute' ? new AbsoluteOrientationSensor({frequency: 60}) : new RelativeOrientationSensor({frequency: 60});
     const mat4 = sensorType === 'absolute' ? this.absoluteMat4 : this.relativeMat4;
     sensor.addEventListener('reading', () => sensor.populateMatrix(mat4));
     sensor.addEventListener('error', sensorErrorEvent =>
       this.showError(sensorErrorEvent.error.message)
     );
-    if ('absolute') {
+    if (sensorType === 'absolute') {
       this.absoluteSensor = sensor;
-      this.absoluteSensor.start();
     } else {
       this.relativeSensor = sensor;
-      this.relativeSensor.start();
     }
+    sensor.start();
   }
 }
