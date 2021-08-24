268c15cfc22ac58316c2070b1f460c556495d425
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-24 14:17:15 +0200

    KeyboardLock: simple Keyboard Lock demo

--- src/app/technologies/demos/keyboard-lock/keyboard-lock.component.ts
+++ src/app/technologies/demos/keyboard-lock/keyboard-lock.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -6,5 +6,44 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   templateUrl: './keyboard-lock.component.html',
   styleUrls: ['./keyboard-lock.component.css']
 })
-export class KeyboardLockComponent extends TechnologyDemoComponent {
+export class KeyboardLockComponent extends TechnologyDemoComponent implements OnDestroy {
+  locked = false;
+
+  ngOnDestroy(): void {
+    if (this.locked) {
+      this.unlockKeyboard();
+      this.showError('Keyboard Lock wurde wieder aufgehoben');
+    }
+  }
+
+  lockKeyboard(): void {
+    // @ts-ignore
+    if ('keyboard' in navigator && 'lock' in navigator.keyboard) {
+      // @ts-ignore
+      navigator.keyboard.lock()
+        .then(() => this.locked = true)
+        .catch(err => this.showError(err));
+    }
+  }
+
+  unlockKeyboard(): void {
+    if (this.locked) {
+      // @ts-ignore
+      navigator.keyboard.unlock();
+      this.locked = false;
+    }
+  }
+
+  activateFullscreen(): void {
+    if ('requestFullscreen' in document.documentElement) {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen()
+          .catch(err => this.showError(err));
+      } else {
+        this.showError('Vollbildmodus ist bereits aktiviert.');
+      }
+    } else {
+      this.showNoSupportError('Element.requestFullscreen()');
+    }
+  }
 }
