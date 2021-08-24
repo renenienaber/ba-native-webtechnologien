e2a7a855cf41b8ad74587a174bf8b209477d07a2
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-24 16:43:07 +0200

    PageVisibility: demo with audio

--- src/app/technologies/demos/page-visibility/page-visibility.component.ts
+++ src/app/technologies/demos/page-visibility/page-visibility.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {TechnologyDemoComponent} from '../../technology-demo.component';
+import {fromEvent, Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-page-visibility',
   templateUrl: './page-visibility.component.html',
   styleUrls: ['./page-visibility.component.css']
 })
-export class PageVisibilityComponent implements OnInit {
+export class PageVisibilityComponent extends TechnologyDemoComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('audioElement', { static: false })
+  set audioRef(ref: ElementRef<HTMLAudioElement>) {
+    this.audioElement = ref.nativeElement;
+  }
+  private audioElement: HTMLAudioElement;
 
-  constructor() { }
+  private visibilityChange$: Observable<Event>;
+  private visibilityChangeSubscription: Subscription;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    if ('visibilityState' in document) {
+      this.visibilityChange$ = fromEvent(document, 'visibilitychange');
+      this.visibilityChangeSubscription = this.visibilityChange$.subscribe( () =>
+        this.onVisibilityChange()
+      );
+    } else {
+      this.showNoSupportError('document.visibilityState');
+    }
   }
 
+  ngOnDestroy(): void {
+    if (this.visibilityChangeSubscription) {
+      this.visibilityChangeSubscription.unsubscribe();
+    }
+  }
+
+  private onVisibilityChange(): void {
+    if (document.hidden) {
+      this.audioElement.pause();
+    } else {
+      this.audioElement.play().then(() => undefined);
+    }
+  }
 }
