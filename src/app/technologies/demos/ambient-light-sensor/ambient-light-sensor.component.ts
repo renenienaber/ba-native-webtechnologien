cdfd53ac5f8700a50829f68f0cbc0762e37cf8bf
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-25 16:58:01 +0200

    AmbientLightSensor: simple demo

--- src/app/technologies/demos/ambient-light-sensor/ambient-light-sensor.component.ts
+++ src/app/technologies/demos/ambient-light-sensor/ambient-light-sensor.component.ts
@@ -8,4 +8,23 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   ]
 })
 export class AmbientLightSensorComponent extends TechnologyDemoComponent {
+  private sensor: any;
+  result: number;
+
+  initAndStartSensor(): void {
+    if ('AmbientLightSensor' in window) {
+      // @ts-ignore
+      this.sensor = new AmbientLightSensor({frequency: 10});
+      this.sensor.addEventListener('reading', () => {
+          this.result = this.sensor.illuminance;
+        }
+      );
+      this.sensor.addEventListener('error', sensorErrorEvent =>
+        this.showError(sensorErrorEvent.error.message)
+      );
+      this.sensor.start();
+    } else {
+      this.showNoSupportError('window.AmbientLightSensor');
+    }
+  }
 }
