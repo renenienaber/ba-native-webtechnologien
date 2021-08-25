8c4c61084b9c6d2fd1f79d9860f2f77ad7130ea7
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-25 17:33:17 +0200

    ProximitySensor: simple demo

--- src/app/technologies/demos/proximity-sensor/proximity-sensor.component.ts
+++ src/app/technologies/demos/proximity-sensor/proximity-sensor.component.ts
@@ -8,4 +8,32 @@ import {TechnologyDemoComponent} from '../../technology-demo.component';
   ]
 })
 export class ProximitySensorComponent extends TechnologyDemoComponent {
+  private sensor: any;
+  results: ProximityData;
+
+  initAndStartSensor(): void {
+    if ('ProximitySensor' in window) {
+      // @ts-ignore
+      this.sensor = new ProximitySensor({frequency: 60});
+      this.sensor.addEventListener('reading', () => {
+          this.results = {
+            distance: this.sensor.distance,
+            max: this.sensor.max,
+            near: this.sensor.near};
+        }
+      );
+      this.sensor.addEventListener('error', sensorErrorEvent =>
+        this.showError(sensorErrorEvent.error.message)
+      );
+      this.sensor.start();
+    } else {
+      this.showNoSupportError('window.ProximitySensor');
+    }
+  }
+}
+
+interface ProximityData {
+  distance?: number;
+  max?: number;
+  near?: boolean;
 }
