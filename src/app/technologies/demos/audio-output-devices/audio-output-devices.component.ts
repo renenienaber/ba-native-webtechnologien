0992a6d185c45ef285a8c9f2b6ed83a9a24b8516
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-08-25 20:32:52 +0200

    AudioOutputDevices: demo with setSinkId on Audio-Element

--- src/app/technologies/demos/audio-output-devices/audio-output-devices.component.ts
+++ src/app/technologies/demos/audio-output-devices/audio-output-devices.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
   selector: 'app-audio-output-devices',
-  templateUrl: './audio-output-devices.component.html',
-  styles: [
-  ]
+  templateUrl: './audio-output-devices.component.html'
 })
-export class AudioOutputDevicesComponent implements OnInit {
+export class AudioOutputDevicesComponent extends TechnologyDemoComponent implements OnInit {
+  @ViewChild('audioElement', { static: false })
+  set audioRef(ref: ElementRef<HTMLAudioElement>) {
+    this.audioElement = ref.nativeElement;
+  }
+  private audioElement: HTMLAudioElement;
 
-  constructor() { }
+  audioDevices: MediaDeviceInfo[] = [];
 
   ngOnInit(): void {
+    if ('mediaDevices' in navigator && 'enumerateDevices' in navigator.mediaDevices) {
+      navigator.mediaDevices.enumerateDevices()
+        .then(res => this.audioDevices = res.filter(d => d.kind === 'audiooutput'))
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('navigator.mediaDevices.enumerateDevices() (zur Ermittlung der Geräte)');
+    }
+  }
+
+  onSelectionChanged(audioDevice: MediaDeviceInfo): void {
+    if ('setSinkId' in this.audioElement) {
+      // @ts-ignore
+      this.audioElement.setSinkId(audioDevice.deviceId)
+        .catch(err => this.showError(err));
+    } else {
+      this.showNoSupportError('HTMLMediaElement.setSinkId');
+    }
   }
 
 }
