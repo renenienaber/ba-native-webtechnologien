a44cf72679f7b25f215c2732ad05ef08e17d9755
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-09-10 19:55:40 +0200

    WebOTP: redesigned demo

--- src/app/technologies/demos/webotp/webotp.component.ts
+++ src/app/technologies/demos/webotp/webotp.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Inject, ViewChild} from '@angular/core';
+import {Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 import {DOCUMENT} from '@angular/common';
 
@@ -6,7 +6,7 @@ import {DOCUMENT} from '@angular/common';
   selector: 'app-webotp',
   templateUrl: './webotp.component.html'
 })
-export class WebotpComponent extends TechnologyDemoComponent implements AfterViewInit {
+export class WebotpComponent extends TechnologyDemoComponent {
   @ViewChild('formElement', { static: false })
   set formRef(ref: ElementRef<HTMLFormElement>) {
     this.formElement = ref.nativeElement;
@@ -23,41 +23,45 @@ export class WebotpComponent extends TechnologyDemoComponent implements AfterVie
   CODE = '123456';
 
   private abortController: AbortController;
+  isActive = false;
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     super();
     this.hosturl = document.location.hostname;
   }
 
-  ngAfterViewInit(): void {
+  startOTPRetrieval(): void {
     if ('OTPCredential' in window) {
-      this.init();
+      this.abortController = new AbortController();
+
+      const requestOptions: CredentialRequestOptions = {
+        // @ts-ignore
+        otp: { transport: ['sms'] },
+        signal: this.abortController.signal
+      };
+
+      navigator.credentials.get(requestOptions)
+        .then(otp => {
+          this.isActive = false;
+
+          // @ts-ignore
+          const code = otp.code;
+          this.showError(`SMS-OTP wurde empfangen: ${code}`);
+          this.inputElement.value = code;
+          this.formElement.submit();
+        })
+        .catch(err => {
+          this.isActive = false;
+          this.showError(err);
+        });
+
+      this.isActive = true;
+    } else {
+      this.showNoSupportError('window.OTPCredential');
     }
   }
 
-  private init(): void {
-    this.abortController = new AbortController();
-
-    this.formElement.addEventListener('submit', () => this.onFormSubmit);
-
-    const requestOptions: CredentialRequestOptions = {
-      // @ts-ignore
-      otp: { transport: ['sms'] },
-      signal: this.abortController.signal
-    };
-    navigator.credentials.get(requestOptions)
-      .then(otp => {
-        // @ts-ignore
-        const code = otp.code;
-
-        this.showError(`SMS-OTP wurde empfangen: ${code}`);
-        this.inputElement.value = code;
-        this.formElement.submit();
-      })
-      .catch(err => this.showError(err));
-  }
-
-  private onFormSubmit(): void {
+  onFormSubmit(): void {
     if (this.abortController) {
       this.abortController.abort();
     }
