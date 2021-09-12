dc180cff6942698b60ed789728047abec8fc56b6
René Nienaber <37839940+renenienaber@users.noreply.github.com>
2021-09-12 11:28:04 +0200

    fixed display of ComputePressure data, using non-spec stop()-method onDestroy if unobserve()-method doesn't exist

--- src/app/technologies/demos/compute-pressure/compute-pressure.component.ts
+++ src/app/technologies/demos/compute-pressure/compute-pressure.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {TechnologyDemoComponent} from '../../technology-demo.component';
 
 @Component({
@@ -13,17 +13,24 @@ export class ComputePressureComponent extends TechnologyDemoComponent implements
   };
   results: ComputePressureData;
 
+  constructor(private cdRef: ChangeDetectorRef) {
+    super();
+  }
+
   ngOnDestroy() {
     if (this.computePressureObserver) {
-      // @ts-ignore
-      this.computePressureObserver.unobserve();
+      if ('unobserve' in this.computePressureObserver) {
+        this.computePressureObserver.unobserve();
+      } else {
+        this.computePressureObserver.stop();
+      }
     }
   }
 
   initAndObserve(): void {
     if ('ComputePressureObserver' in window) {
       // @ts-ignore
-      this.computePressureObserver = new ComputePressureObserver(this.callback, this.options);
+      this.computePressureObserver = new ComputePressureObserver(update => this.callback(update), this.options);
       this.computePressureObserver.observe();
     } else {
       this.showNoSupportError('window.ComputePressureObserver');
@@ -35,6 +42,7 @@ export class ComputePressureComponent extends TechnologyDemoComponent implements
       cpuSpeed: update.cpuSpeed,
       cpuUtilization: update.cpuUtilization
     };
+    this.cdRef.detectChanges();
   }
 }
 
